8c6cc6189d70d16ecc7b62925925c7b7f7652558
CHO, NAEUN <jo044879@gmail.com>
2026-08-09 01:17:27 +0900

    접근권환 최신화

--- src/main/java/com/lionapple/common/auth/WebConfig.java
+++ src/main/java/com/lionapple/common/auth/WebConfig.java
@@ -44,8 +44,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:5173",
-                        "https://hipapple-front.pages.dev"
+                        "https://hip-apple.vercel.app",
+                        "http://localhost:5173"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "Accept")
